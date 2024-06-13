301ce5aa2a43e4d7a639da6d5b839a4ea0e42b04
Max Bonetti <maxwell.bonetti@gmail.com>
2024-06-13 12:04:39 -0500

    fixed thought functionality to redner onto homepage. Home route was inside login route.

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutation';
 import Auth from '../../utils/auth';
+import ThoughtList from '../ThoughtList';
 
 
 const Login = (props) => {
@@ -48,12 +49,12 @@ const Login = (props) => {
         <div className="card">
           <h4 className="card-header bg-dark text-light p-2">Login</h4>
           <div className="card-body">
-            {data ? (
+            {/* {data ? (
               <p>
                 Success! You may now head{' '}
                 <Link to="/">back to the homepage.</Link>
               </p>
-            ) : (
+            ) : ( */}
               <form onSubmit={handleFormSubmit}>
                 <input
                   className="form-input"
@@ -79,7 +80,8 @@ const Login = (props) => {
                   Submit
                 </button>
               </form>
-            )}
+            {/* )} */}
+            
 
             {error && (
               <div className="my-3 p-3 bg-danger text-white">
